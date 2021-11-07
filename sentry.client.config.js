ccfa84084840053e31983b75f3f69ae3919a6c75
librra <91788409+librra@users.noreply.github.com>
2021-11-07 10:46:55 +0700

    Update sentry.client.config.js

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -3,6 +3,6 @@ import * as Sentry from "@sentry/nextjs";
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 Sentry.init({
-  dsn: SENTRY_DSN || "https://88092e8a40ad4d44aafc689d5681a09e@o993759.ingest.sentry.io/5951862",
+  dsn: SENTRY_DSN || "https://4d5c1157f3fb4bca98f3b5b043d52300@o1062193.ingest.sentry.io/6052592",
   tracesSampleRate: 0.2,
 });
